113a008ca84b9eede427152284623b82234d85e8
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 22:30:18 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,10 +17,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]\w*./)
+  if text.match(/^[A-Z].+[\.!?]$/)
     return true
-  elsif text.match(/^[A-Z]\w*(?<!.)$/)
-    return false
   else
     return false
   end
